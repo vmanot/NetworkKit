142bad63038d2131c7e3811e8713f4e855e9e6cd
Vatsal Manot <vatsal.manot@yahoo.com>
2020-03-28 23:31:50 +0530

    Update package

--- Sources/Intramodular/HTTP/Multipart/HTTPMultipartRequestContentEntity.swift
+++ Sources/Intramodular/HTTP/Multipart/HTTPMultipartRequestContentEntity.swift
@@ -11,11 +11,13 @@ public protocol HTTPMultipartRequestContentEntity: CustomStringConvertible {
 }
 
 extension HTTPMultipartRequestContentEntity {
-    
     /// Sets an attribute for a header field, like the "name" attribute for the Content-Disposition header.
     /// If the specified header is not defined for this entity, the attribute is ignored.
     /// If a value was previously set for the given attribute, that value is replaced with the given value.
-    public mutating func setAttribute(attribute: String, value: String?, forHeaderField headerName: String) {
+    public mutating func setAttribute(
+        attribute: String, value: String?,
+        forHeaderField headerName: String
+    ) {
         if let value = value {
             self.headers[headerName]?.attributes[attribute] = value
         } else {
@@ -23,17 +25,19 @@ extension HTTPMultipartRequestContentEntity {
         }
     }
     
-    /// Sets a value for a header field. If a value was previously set for the given header, that value is replaced with
-    /// the given value.
-    public mutating func setValue(_ value: String?, forHeaderField headerName: String) {
+    /// Sets a value for a header field. If a value was previously set for the given header, that value is replaced with the given value.
+    public mutating func setValue(
+        _ value: String?,
+        forHeaderField headerName: String
+    ) {
         if let value = value {
-            if self.headers[headerName] != nil {
-                self.headers[headerName]?.value = value
+            if headers[headerName] != nil {
+                headers[headerName]?.value = value
             } else {
-                self.headers.append(HTTPMultipartRequestHeader(name: headerName, value: value))
+                headers.append(HTTPMultipartRequestHeader(name: headerName, value: value))
             }
         } else {
-            self.headers.remove(headerName)
+            headers.remove(headerName)
         }
     }
 }
