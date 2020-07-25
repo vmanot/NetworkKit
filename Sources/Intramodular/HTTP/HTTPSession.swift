7cd886c64ab6b35617fb2b6db3d123547dc17e0f
Vatsal Manot <vatsal.manot@yahoo.com>
2020-07-26 02:51:04 +0530

    Update package

--- Sources/Intramodular/HTTP/HTTPSession.swift
+++ Sources/Intramodular/HTTP/HTTPSession.swift
@@ -10,19 +10,19 @@ import Swallow
 public struct HTTPSession: Identifiable, Initiable, RequestSession {
     public let cancellables = Cancellables()
     public let id = UUID()
-
+    
     private let base: URLSession
     
     public init() {
         self.base = URLSession(configuration: .default)
     }
     
-    public func task(with request: HTTPRequest) -> AnyPublisher<HTTPRequest.Response, HTTPRequest.Error> {
+    public func task(with request: HTTPRequest) -> AnyTask<HTTPRequest.Response, HTTPRequest.Error> {
         do {
             return try base.dataTaskPublisher(for: request)
                 .map({ HTTPRequest.Response(data: $0.data, urlResponse: $0.response as! HTTPURLResponse) })
                 .mapError(HTTPRequestError.system)
-                .eraseToAnyPublisher()
+                .eraseToTask()
         } catch {
             return .failure(HTTPRequestError.system(error))
         }
