a2862efa59781dc6360d99c676732163a0f343e8
Vatsal Manot <vatsal.manot@yahoo.com>
2020-08-13 16:31:28 +0530

    Update package

--- Package.swift
+++ Package.swift
@@ -14,10 +14,10 @@ let package = Package(
         .library(name: "NetworkKit", targets: ["NetworkKit"])
     ],
     dependencies: [
-        .package(url: "git@github.com:vmanot/API.git", .branch("master")),
-        .package(url: "git@github.com:vmanot/Merge.git", .branch("master")),
-        .package(url: "git@github.com:vmanot/Swallow.git", .branch("master")),
-        .package(url: "git@github.com:vmanot/Task.git", .branch("master"))
+        .package(url: "https://github.com/vmanot/API.git", .branch("master")),
+        .package(url: "https://github.com/vmanot/Merge.git", .branch("master")),
+        .package(url: "https://github.com/vmanot/Swallow.git", .branch("master")),
+        .package(url: "https://github.com/vmanot/Task.git", .branch("master"))
     ],
     targets: [
         .target(name: "NetworkKit", dependencies: ["API", "Merge", "Swallow", "Task"], path: "Sources"),
