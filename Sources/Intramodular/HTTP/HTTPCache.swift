b439901d170d1f083a495053a05bbdcdf60b88c0
Vatsal Manot <vatsal.manot@yahoo.com>
2022-06-20 17:19:49 +0530

    Update package

--- Sources/Intramodular/HTTP/HTTPCache.swift
+++ Sources/Intramodular/HTTP/HTTPCache.swift
@@ -15,32 +15,21 @@ public struct HTTPCache: KeyedCache, Initiable {
         
     }
     
-    public func cache(_ value: Value, forKey key: Key) -> AnySingleOutputPublisher<Void, Error> {
-        Result {
-            try base.storeCachedResponse(.init(value), for: .init(key))
-        }
-        .publisher
-        .eraseToAnySingleOutputPublisher()
+    public func cache(_ value: Value, forKey key: Key) async throws {
+        try base.storeCachedResponse(.init(value), for: .init(key))
     }
     
-    public func decacheInMemoryValue(forKey key: Key) throws -> Value? {
+    public func retrieveInMemoryValue(forKey key: Key) throws -> Value? {
+        
         // try base.cachedResponse(for: try URLRequest(key)).map(HTTPResponse.init)
         return nil // FIXME: The cache doesn't seem to ever expire.
     }
     
-    public func removeCachedValue(forKey key: Key) -> AnySingleOutputPublisher<Void, Error> {
-        Result {
-            try base.removeCachedResponse(for: .init(key))
-        }
-        .publisher
-        .eraseToAnySingleOutputPublisher()
+    public func removeCachedValue(forKey key: Key) async throws {
+        try base.removeCachedResponse(for: .init(key))
     }
     
-    public func removeAllCachedValues() -> AnySingleOutputPublisher<Void, Error> {
-        Result {
-            base.removeAllCachedResponses()
-        }
-        .publisher
-        .eraseToAnySingleOutputPublisher()
+    public func removeAllCachedValues() async throws {
+        base.removeAllCachedResponses()
     }
 }
