1b64759a467b429cf0d093ca2f06da9178f356f6
Vatsal Manot <vatsal.manot@yahoo.com>
2022-12-23 01:07:53 +0530

    Update package

--- Sources/Intramodular/HTTP/HTTPRepository.swift
+++ Sources/Intramodular/HTTP/HTTPRepository.swift
@@ -6,38 +6,38 @@ import API
 import Merge
 import Swift
 
-public protocol HTTPRepository: Repository where Session == HTTPSession {
+public protocol HTTPClient: Client where Session == HTTPSession {
     associatedtype SessionCache = HTTPCache
 }
 
 // MARK: - Implementation -
 
-private var _HTTPRepository_session_objcAssociationKey: UInt8 = 0
-private var _HTTPRepository_sessionCache_objcAssociationKey: UInt8 = 0
-private var _HTTPRepository_logger_objcAssociationKey: UInt8 = 0
+private var _HTTPClient_session_objcAssociationKey: UInt8 = 0
+private var _HTTPClient_sessionCache_objcAssociationKey: UInt8 = 0
+private var _HTTPClient_logger_objcAssociationKey: UInt8 = 0
 
-extension HTTPRepository  {
+extension HTTPClient  {
     public var session: HTTPSession {
-        if let result = objc_getAssociatedObject(self, &_HTTPRepository_session_objcAssociationKey) as? HTTPSession {
+        if let result = objc_getAssociatedObject(self, &_HTTPClient_session_objcAssociationKey) as? HTTPSession {
             return result
         } else {
             let result = HTTPSession()
             
-            objc_setAssociatedObject(self, &_HTTPRepository_session_objcAssociationKey, result, .OBJC_ASSOCIATION_RETAIN)
+            objc_setAssociatedObject(self, &_HTTPClient_session_objcAssociationKey, result, .OBJC_ASSOCIATION_RETAIN)
             
             return result
         }
     }
 }
 
-extension HTTPRepository where SessionCache == HTTPCache {
+extension HTTPClient where SessionCache == HTTPCache {
     public var sessionCache: SessionCache {
-        if let result = objc_getAssociatedObject(self, &_HTTPRepository_sessionCache_objcAssociationKey) as? SessionCache {
+        if let result = objc_getAssociatedObject(self, &_HTTPClient_sessionCache_objcAssociationKey) as? SessionCache {
             return result
         } else {
             let result = SessionCache()
             
-            objc_setAssociatedObject(self, &_HTTPRepository_sessionCache_objcAssociationKey, result, .OBJC_ASSOCIATION_RETAIN)
+            objc_setAssociatedObject(self, &_HTTPClient_sessionCache_objcAssociationKey, result, .OBJC_ASSOCIATION_RETAIN)
             
             return result
         }
